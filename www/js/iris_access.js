679bb9592be0dc8c86d486039cca6dddba18ff3f
ArchieGoodwin <nerowolfe@me.com>
2016-06-09 12:06:28 +0300

    clearUI methods fixes

--- www/js/iris_access.js
+++ www/js/iris_access.js
@@ -5,6 +5,9 @@ var IrisAccess = {
     getIris: function(success, failure, args){
         cordova.exec(success, failure, "IrisAccess", "getIris", [args]);
     }
+    clearUI: function(success, failure){
+        cordova.exec(success, failure, "IrisAccess", "clearUI");
+    }
     };
 
 module.exports = IrisAccess;
